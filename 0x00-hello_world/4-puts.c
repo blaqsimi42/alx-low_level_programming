d247d778882939d7736faec07c735b6779e043b3
blaqsimi42 <cimee464@gmail.com>
2023-09-08 19:50:36 +0000

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - this is the main function
- * Return: always 0
+ * main - this is the main function for program
+ *
+ * Return: always 0 (success);
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle):
-		return (0):
-}			
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is my main function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\" with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
