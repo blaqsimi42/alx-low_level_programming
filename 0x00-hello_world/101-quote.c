f43dde5cc65f11d0382156c80c452bc4d75577a7
blaqsimi42 <cimee464@gmail.com>
2023-12-14 20:43:08 +0000

    hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - this is my main program function
  *
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59, 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
 	return (1);
 }
