20e609d945865a40bc209585748f3c904e054910
blaqsimi42 <cimee464@gmail.com>
2024-01-08 17:27:36 +0000

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the pointer value to char
+ *
+ * @s: string double pointer
+ * @to: the string pointer
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
